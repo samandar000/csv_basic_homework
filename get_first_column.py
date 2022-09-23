7be6d259e7456511f692c267ab0c02b67689ff6f
Samandar000 <samandarnfh.2020@gmail.com>
2022-09-23 23:17:43 +0500

    04

--- get_first_column.py
+++ get_first_column.py
@@ -6,15 +6,15 @@ def get_first_column(data):
     Return:
         list: First column.
     """
-    list1 = data.split(',')
-    list1 = data.split('\n')
-    return (list1[0])
-
+    data = data.split('\n')
+    l= []
+    for i in data:
+        a = i.split(',')
+        l.append(a[0])
+       
+    return (l)
 data = open('data.csv').read()
-#data = data.split('\n')
-# data = data.split(',')
 
+print(get_first_column(data))
 
-print(get_first_column((data)))
-    
 # Read the csv file
